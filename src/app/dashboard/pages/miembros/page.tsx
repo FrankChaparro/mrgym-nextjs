44752fd06e7775fce1cc6faf27ca2ea88b48e325
AtunN25 <franklin25.2001@gmail.com>
2024-11-06 23:55:04 -0500

    implemented qr generator

--- src/app/dashboard/pages/miembros/page.tsx
+++ src/app/dashboard/pages/miembros/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import QRCode from 'react-qr-code'
+
 
 function Miembros() {
+
+
+
   return (
     <div className="p-4 sm:ml-64">
         <div className="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700 mt-14">
-          miembros
+            <div className='border rounded-lg border-gray-950 p-4 flex-auto w-1/4'>
+              <QRCode value="HSDF234" size={256} bgColor="#282c34" fgColor="#fff" level="H" />
+            </div>
+           
         </div>
       </div>
   )
