de281ef95eb93c6f49f1a2a2bd095736b81cbdb1
AtunN25 <franklin25.2001@gmail.com>
2024-11-06 23:08:05 -0500

    implemented qr reader

--- src/app/dashboard/pages/asistencia/page.tsx
+++ src/app/dashboard/pages/asistencia/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import Html5QrcodePlugin from './Html5QrcodeScannerPlugin'
+
 
 function Asistencia() {
+
+    const onNewScanResult = (decodedText: string, decodedResult: unknown) => {
+        console.log("Decoded text:", decodedText);
+        console.log("DecodedResult text:", decodedResult);
+
+        if(decodedText){
+          alert("se encontro el codigo")
+        }
+    };
+    
   return (
     <div className="p-4 sm:ml-64">
         <div className="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700 mt-14">
           asistencia
         </div>
+        <Html5QrcodePlugin
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
       </div>
   )
 }
