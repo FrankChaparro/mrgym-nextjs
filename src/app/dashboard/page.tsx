04277e7dc60417a2e2c6514b7ed6052f31e2de88
AtunN25 <franklin25.2001@gmail.com>
2024-11-06 20:18:34 -0500

    finished routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+
+
+function Dashboard() {
+  return (
+    <div>
+      
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700 mt-14">
+          holaaa
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/app/dashboard/pages/asistencia/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Asistencia() {
+  return (
+    <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700 mt-14">
+          asistencia
+        </div>
+      </div>
+  )
+}
+
+export default Asistencia
