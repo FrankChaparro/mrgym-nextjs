2c1fe0ec56427b3155b2e881bbeb814f28d85f09
AtunN25 <franklin25.2001@gmail.com>
2024-11-09 00:14:58 -0500

    added qr generator

--- src/app/dashboard/pages/miembros/page.tsx
+++ src/app/dashboard/pages/miembros/page.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-//import QRCode from 'react-qr-code'
-
 import React, { useEffect, useState } from 'react';
+import QRCode from 'react-qr-code'
 import Table from '@/components/Table'
-import {Client} from '@/Interface/Client'
+import { Client } from '@/Interface/Client'
 import Register from '@/components/Register'
+import Swal from 'sweetalert2';
+
 
 function Miembros() {
 
+
   const [clients, setClients] = useState<Client[]>([]);
+  const [qrDniCliente, setQrDniCliente] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,6 +27,7 @@ function Miembros() {
           });
 
           if (!response.ok) {
+
             throw new Error('Error en la solicitud');
           }
 
@@ -46,47 +50,64 @@ function Miembros() {
     const token = localStorage.getItem('token');
 
     if (token) {
-        try {
-            const response = await fetch('https://mrgymbackendspringboot-production-2dcf.up.railway.app/cliente/agregar', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                },
-                body: JSON.stringify(data),
-            });
+      try {
+        const response = await fetch('https://mrgymbackendspringboot-production-2dcf.up.railway.app/cliente/agregar', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        });
 
-            if (!response.ok) {
-              console.log(JSON.stringify(data))
-                throw new Error('Error en la solicitud');
-                
-            }
-
-            const newClient = await response.json();
-            console.log('Cliente registrado con éxito:', newClient);
-
-            //actualizar la lista de clientes
-            setClients((prevClients) => [...prevClients, newClient]);
-
-        } catch (error) {
-            console.error('Error al registrar el cliente:', error);
+        if (!response.ok) {
+          console.log(JSON.stringify(data))
+          Swal.fire({
+            title: "Error 403!",
+            text: "formato de datos incorrectos!",
+            icon: "error"
+          });
+          throw new Error('Error en la solicitud');
         }
+
+        const newClient = await response.json();
+        Swal.fire({
+          title: "Buen Trabajo!",
+          text: "Cliente registrado con exito!",
+          icon: "success"
+        });
+        console.log('Cliente registrado con éxito:', newClient);
+
+        setQrDniCliente(newClient.dni_cliente);
+
+        //actualizar la lista de clientes
+        setClients((prevClients) => [...prevClients, newClient]);
+
+      } catch (error) {
+        console.error('Error al registrar el cliente:', error);
+      }
     } else {
-        console.log('Token no encontrado');
+      console.log('Token no encontrado');
     }
-};
-  
+  };
+
 
   return (
     <div className="p-4 sm:ml-64">
       <div className=" border-2 border-gray-200  rounded-lg dark:border-gray-700 mt-14">
 
         <div className='border rounded-lg border-gray-950 p-4 flex-auto space-y-2'>
-          {/*<QRCode value="test" size={256} bgColor="#282c34" fgColor="#fff" level="H" />*/}
+
 
           <Register onFormSubmit={RegistrarCliente}></Register>
 
           <Table data={clients}></Table>
+
+          {qrDniCliente && (
+            <div className="mt-4 flex justify-center">
+              <QRCode value={qrDniCliente} size={256} bgColor="#282c34" fgColor="#fff" level="H" />
+            </div>
+          )}
 
         </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState,FormEvent } from 'react';
+import { useState, FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import Swal from 'sweetalert2';
 
@@ -9,13 +9,13 @@ function Page() {
     const [contrasenia, setcontrasenia] = useState<string>('');
     const router = useRouter();
 
-    const handleSubmit = async (e:FormEvent) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         console.log('Usuario:', usuario);
         console.log('Contraseña:', contrasenia);
 
         try {
-            
+
             const response = await fetch('/auth/login', {
                 method: 'POST',
                 headers: {
@@ -27,7 +27,7 @@ function Page() {
                 }),
             });
 
-           
+
             if (!response.ok) {
                 Swal.fire({
                     title: "Error!",
@@ -35,13 +35,13 @@ function Page() {
                     icon: "error"
                 });
                 throw new Error('Hubo un problema con la autenticación');
-            }else{
-                
+            } else {
+
                 Swal.fire({
                     title: "Buen Trabajo!",
                     text: "credenciales correctas!",
                     icon: "success"
-                  });
+                });
                 const data = await response.json();
                 console.log('Respuesta del servidor:', data);
 
@@ -51,9 +51,9 @@ function Page() {
             }
 
 
-            
+
         } catch (error) {
-            console.log(error , 'Algo salió mal');
+            console.log(error, 'Algo salió mal');
         }
     }
 
@@ -65,24 +65,24 @@ function Page() {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Ingresa tu cuenta 
+                                Ingresa tu cuenta
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                                 <div>
-                                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
-                                    <input  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="user" 
-                                    onChange={(e) => setusuario(e.target.value)}
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
+                                    <input className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="user"
+                                        onChange={(e) => setusuario(e.target.value)}
                                     />
                                 </div>
                                 <div>
-                                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-                                    <input type="password"  placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    onChange={(e) => setcontrasenia(e.target.value)}
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+                                    <input type="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        onChange={(e) => setcontrasenia(e.target.value)}
                                     />
                                 </div>
-                               
+
                                 <button type="submit" className="w-full text-white bg-slate-800 border hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                               
+
                             </form>
                         </div>
                     </div>
